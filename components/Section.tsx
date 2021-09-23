c086c3be9d549821a2223223de83e6d0c8a622c6
Derek Pizzolatto <derek.pizzolatto@gmail.com>
2021-09-23 18:18:12 -0500

    update: card backgrounds

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,18 +2,34 @@ import styles from '../styles/Section.module.css';
 
 const Section = ({ section }: any) => {
   const { body } = section;
+  let sectionColor;
+  switch (section.id) {
+    case 0:
+      sectionColor = '#ff000055';
+      break;
+    case 1:
+      sectionColor = '#00ff0055';
+      break;
+    case 2:
+      sectionColor = '#0000ff55';
+      break;
+    default:
+      sectionColor = '#ffffff44';
+  }
   return (
     <>
       <div
         className={styles.headerBar}
-        style={{zIndex: section.id + 1}}
+        style={{zIndex: section.id + 2}}
       >
         <div>{section.title.toUpperCase()}</div>
       </div>
       <div
         id={section.title}
         className={styles.card}
-        style={{backgroundImage: `url(${section.background})`}}
+        style={{
+          backgroundImage: `linear-gradient(${sectionColor}, ${sectionColor}), url(${section.background})`,
+        }}
       >
         <div className={styles.cardText}>{body}</div>
         <div className={styles.filter} />
--- data.js
+++ data.js
@@ -3,19 +3,19 @@ export const sections = [
     id: 0,
     title: 'about',
     body: 'Hey there! My name is Derek and I\'m a web developer from Louisiana.', 
-    background: '/images/about.jpg',
+    background: '/images/cardBackground.png',
   },
   {
     id: 1,
     title: 'works',
     body: 'Here is a list of the different things I\'ve worked on: To Be Completed',
-    background: '/images/works.jpg',
+    background: '/images/cardBackground.png',
   },
   {
     id: 2,
     title: 'contact',
     body: 'You can contact me via my email address: derek@notmyrealemail.com',
-    background: '/images/contact.jpg',
+    background: '/images/cardBackground.png',
   },
   {
     id: 3,
