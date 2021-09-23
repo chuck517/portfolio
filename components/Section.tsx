c237bcef0a489211864ca0067b5bff4ceb140a3c
Derek Pizzolatto <derek.pizzolatto@gmail.com>
2021-09-23 17:36:08 -0500

    update: styling

--- components/Section.tsx
+++ components/Section.tsx
@@ -4,9 +4,19 @@ const Section = ({ section }: any) => {
   const { body } = section;
   return (
     <>
-      <div className={styles.headerBar} style={{zIndex: section.id + 1}}>{section.title.toUpperCase()}</div>
-      <div id={section.title} className={styles.card} style={{backgroundImage: `radial-gradient(#000000bb, #000000bb, #000000bb, #000000bb, #55555588), url(${section.background})`}}>
+      <div
+        className={styles.headerBar}
+        style={{zIndex: section.id + 1}}
+      >
+        <div>{section.title.toUpperCase()}</div>
+      </div>
+      <div
+        id={section.title}
+        className={styles.card}
+        style={{backgroundImage: `url(${section.background})`}}
+      >
         <div className={styles.cardText}>{body}</div>
+        <div className={styles.filter} />
       </div>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Nav from '../components/Nav';
 import ProfilePic from '../components/ProfilePic';
 import Section from '../components/Section'
 import { sections } from '../data';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 
   return (
     <div>
       <ProfilePic />
-      <Nav />
+      {/* <Nav /> */}
       {
         sections.map(section => <Section key={section.id} section={section}/>)
       }
